d4fd947f2c6501ca079fb727dfba452bc53dde35
Knar <knarxr@protonmail.com>
2023-11-29 16:59:25 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,29 @@
 fn main() {
-    let names = vec!["Bob", "Frank", "Ferris"];
+    let number = 13;
+    // TODO ^ Try different values for `number`
 
-    for name in names.iter() {
-        match name {
-            &"Ferris" => println!("There is a rustacean among us!"),
-            // TODO ^ Try deleting the & and matching just "Ferris"
-            _ => println!("Hello {}", name),
-        }
+    println!("Tell me about {}", number);
+    match number {
+        // Match a single value
+        1 => println!("One!"),
+        // Match several values
+        2 | 3 | 5 | 7 | 11 | 13 => println!("This is a prime"),
+        // TODO ^ Try adding 13 to the list of prime values
+        // Match an inclusive range
+        13..=19 => println!("A teen"),
+        // Handle the rest of cases
+        _ => println!("Ain't special"),
+        // TODO ^ Try commenting out this catch-all arm
     }
 
-    println!("names: {:?}", names);
+    let boolean = true;
+    // Match is an expression too
+    let binary = match boolean {
+        // The arms of a match must cover all the possible values
+        false => 0,
+        true => 1,
+        // TODO ^ Try commenting out one of these arms
+    };
 
-    let names = vec!["Bob", "Frank", "Ferris"];
-
-    for name in names.into_iter() {
-        match name {
-            "Ferris" => println!("There is a rustacean among us!"),
-            _ => println!("Hello {}", name),
-        }
-    }
-
-    //println!("names: {:?}", names);
-    // FIXME ^ Comment out this line
-
-    let mut names = vec!["Bob", "Frank", "Ferris"];
-
-    for name in names.iter_mut() {
-        *name = match name {
-            &mut "Ferris" => "There is a rustacean among us!",
-            _ => "Hello",
-        }
-    }
-
-    println!("names: {:?}", names);
+    println!("{} -> {}", boolean, binary);
 }
