e36c63b7fb3f95a6dc8324f9277e8eccb4c6fe5b
Knar <knarxr@protonmail.com>
2023-12-06 16:56:20 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 fn main() {
-    // A counter variable
-    let mut n = 1;
+    let names = vec!["Bob", "Frank", "Ferris"];
 
-    // Loop while `n` is less than 101
-    while n < 101 {
-        if n % 15 == 0 {
-            println!("fizzbuzz");
-        } else if n % 3 == 0 {
-            println!("fizz");
-        } else if n % 5 == 0 {
-            println!("buzz");
-        } else {
-            println!("{}", n);
+    for name in names.iter() {
+        match name {
+            &"Ferris" => println!("There is a rustacean among us!"),
+            // TODO ^ Try deleting the & and matching just "Ferris"
+            _ => println!("Hello {}", name),
         }
-
-        // Increment counter
-        n += 1;
     }
+
+    println!("names: {:?}", names);
+
+    let names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.into_iter() {
+        match name {
+            "Ferris" => println!("There is a rustacean among us!"),
+            _ => println!("Hello {}", name),
+        }
+    }
+
+    //println!("names: {:?}", names);
+    // FIXME ^ Comment out this line
+
+    let mut names = vec!["Bob", "Frank", "Ferris"];
+
+    for name in names.iter_mut() {
+        *name = match name {
+            &mut "Ferris" => "There is a rustacean among us!",
+            _ => "Hello",
+        }
+    }
+
+    println!("names: {:?}", names);
 }
