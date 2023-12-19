2242e68d8d001c7f0f82d3b73055ee1f943e835d
Knar <knarxr@protonmail.com>
2023-12-18 21:12:50 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,35 @@
 fn main() {
-    let _immutable_binding = 1;
-    let mut mutable_binding = 1;
+    // This binding lives in the main function
+    let long_lived_binding = 1;
 
-    println!("Before mutation: {}", mutable_binding);
+    // This is a block, and has a smaller scope than the main function
+    {
+        // This binding only exists in this block
+        let short_lived_binding = 2;
 
-    // Ok
-    mutable_binding += 1;
+        println!("inner short: {}", short_lived_binding);
+    }
+    // End of the block
 
-    println!("After mutation: {}", mutable_binding);
+    // Error! `short_lived_binding` doesn't exist in this scope
+    //println!("outer short: {}", short_lived_binding);
+    // FIXME ^ Comment out this line
 
-    // Error! Cannot assign a new value to an immutable variable
-    // _immutable_binding += 1;
+    println!("outer long: {}", long_lived_binding);
+
+    let shadowed_binding = 1;
+
+    {
+        println!("before being shadowed: {}", shadowed_binding);
+
+        // This binding *shadows* the outer one
+        let shadowed_binding = "abc";
+
+        println!("shadowed in inner block: {}", shadowed_binding);
+    }
+    println!("outside inner block: {}", shadowed_binding);
+
+    // This binding *shadows* the previous binding
+    let shadowed_binding = "DEF";
+    println!("shadowed in outer block: {}", shadowed_binding);
 }
