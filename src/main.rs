1785a39821d2c3ab81183bb386188a0c87b2cef8
Knar <knarxr@protonmail.com>
2023-12-20 21:48:58 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
-use std::convert::Into;
+use std::convert::From;
 
 #[derive(Debug)]
 struct Number {
     value: i32,
 }
 
-impl Into<Number> for i32 {
-    fn into(self) -> Number {
-        Number { value: self }
+// Define `From`
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
     }
 }
 
 fn main() {
     let int = 5;
-    // Try removing the type annotation
+    // use `Into`
     let num: Number = int.into();
     println!("My number is {:?}", num);
 }
