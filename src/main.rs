85174b81bbd9317ad666ce24fc1f8daeb80cef01
Knar <knarxr@protonmail.com>
2023-12-28 21:47:20 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
-// `NanoSecond`, `Inch`, and `U64` are new names for `u64`.
-type NanoSecond = u64;
-type Inch = u64;
-type U64 = u64;
+use std::convert::From;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
 
 fn main() {
-    // `NanoSecond` = `Inch` = `U64` = `u64`.
-    let nanoseconds: NanoSecond = 5;
-    let inches: Inch = 2 as U64;
-
-    // Note that type aliases *don't* provide any extra type safety, because
-    // aliases are *not* new types
-    println!(
-        "{} nanoseconds + {} inches = {} unit?",
-        nanoseconds,
-        inches,
-        nanoseconds + inches
-    );
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
 }
