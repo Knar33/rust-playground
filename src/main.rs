e9572c1d458b89c8bb0002b7bfb375051ca00820
Knar <knarxr@protonmail.com>
2024-01-09 21:18:24 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,23 @@
 fn main() {
-    // This binding lives in the main function
-    let long_lived_binding = 1;
+    // Declare a variable binding
+    let a_binding;
 
-    // This is a block, and has a smaller scope than the main function
     {
-        // This binding only exists in this block
-        let short_lived_binding = 2;
+        let x = 2;
 
-        println!("inner short: {}", short_lived_binding);
+        // Initialize the binding
+        a_binding = x * x;
     }
-    // End of the block
 
-    // Error! `short_lived_binding` doesn't exist in this scope
-    //println!("outer short: {}", short_lived_binding);
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // Error! Use of uninitialized binding
+    //println!("another binding: {}", another_binding);
     // FIXME ^ Comment out this line
 
-    println!("outer long: {}", long_lived_binding);
+    another_binding = 1;
 
-    let shadowed_binding = 1;
-
-    {
-        println!("before being shadowed: {}", shadowed_binding);
-
-        // This binding *shadows* the outer one
-        let shadowed_binding = "abc";
-
-        println!("shadowed in inner block: {}", shadowed_binding);
-    }
-    println!("outside inner block: {}", shadowed_binding);
-
-    // This binding *shadows* the previous binding
-    let shadowed_binding = "DEF";
-    println!("shadowed in outer block: {}", shadowed_binding);
+    println!("another binding: {}", another_binding);
 }
