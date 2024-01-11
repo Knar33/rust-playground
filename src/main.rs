73f6e5a4053f4b0eb43d794db038a0e8911f25c0
Knar <knarxr@protonmail.com>
2024-01-11 17:07:14 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,37 @@
+// `allow` required to silence warnings because only
+// one variant is used.
+#[allow(dead_code)]
+enum Color {
+    // These 3 are specified solely by their name.
+    Red,
+    Blue,
+    Green,
+    // These likewise tie `u32` tuples to different names: color models.
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
 fn main() {
-    // Try changing the values in the array, or make it a slice!
-    let array = [3, -2, 6];
+    let color = Color::RGB(122, 17, 40);
+    // TODO ^ Try different variants for `color`
 
-    match array {
-        // Binds the second and the third elements to the respective variables
-        [0, second, third] => println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
-
-        // Single values can be ignored with _
-        [1, _, third] => println!(
-            "array[0] = 1, array[2] = {} and array[1] was ignored",
-            third
+    println!("What color is it?");
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red => println!("The color is Red!"),
+        Color::Blue => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) => println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) => println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) => println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) => println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) => println!(
+            "Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+            c, m, y, k
         ),
-
-        // You can also bind some and ignore the rest
-        [-1, second, ..] => println!(
-            "array[0] = -1, array[1] = {} and all the other ones were ignored",
-            second
-        ),
-        // The code below would not compile
-        // [-1, second] => ...
-
-        // Or store them in another array/slice (the type depends on
-        // that of the value that is being matched against)
-        [3, second, tail @ ..] => println!(
-            "array[0] = 3, array[1] = {} and the other elements were {:?}",
-            second, tail
-        ),
-
-        // Combining these patterns, we can, for example, bind the first and
-        // last values, and store the rest of them in a single array
-        [first, middle @ .., last] => println!(
-            "array[0] = {}, middle = {:?}, array[2] = {}",
-            first, middle, last
-        ),
+        // Don't need another arm because all variants have been examined
     }
 }
