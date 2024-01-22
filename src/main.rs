f3777ef6c044751386b323427dc693174ca77d6a
Knar <knarxr@protonmail.com>
2024-01-22 18:13:29 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
+// Unlike C/C++, there's no restriction on the order of function definitions
 fn main() {
-    // Make `optional` of type `Option<i32>`
-    let mut optional = Some(0);
+    // We can use this function here, and define it somewhere later
+    fizzbuzz_to(100);
+}
 
-    // This reads: "while `let` destructures `optional` into
-    // `Some(i)`, evaluate the block (`{}`). Else `break`.
-    while let Some(i) = optional {
-        if i > 9 {
-            println!("Greater than 9, quit!");
-            optional = None;
-        } else {
-            println!("`i` is `{:?}`. Try again.", i);
-            optional = Some(i + 1);
-        }
-        // ^ Less rightward drift and doesn't require
-        // explicitly handling the failing case.
+// Function that returns a boolean value
+fn is_divisible_by(lhs: u32, rhs: u32) -> bool {
+    // Corner case, early return
+    if rhs == 0 {
+        return false;
     }
-    // ^ `if let` had additional optional `else`/`else if`
-    // clauses. `while let` does not have these.
+
+    // This is an expression, the `return` keyword is not necessary here
+    lhs % rhs == 0
+}
+
+// Functions that "don't" return a value, actually return the unit type `()`
+fn fizzbuzz(n: u32) -> () {
+    if is_divisible_by(n, 15) {
+        println!("fizzbuzz");
+    } else if is_divisible_by(n, 3) {
+        println!("fizz");
+    } else if is_divisible_by(n, 5) {
+        println!("buzz");
+    } else {
+        println!("{}", n);
+    }
+}
+
+// When a function returns `()`, the return type can be omitted from the
+// signature
+fn fizzbuzz_to(n: u32) {
+    for n in 1..=n {
+        fizzbuzz(n);
+    }
 }
