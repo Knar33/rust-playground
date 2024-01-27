385af691188691b5667c57408aff87a0c824525c
Knar <knarxr@protonmail.com>
2024-01-26 21:19:51 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,17 @@
 fn main() {
-    // Declare a variable binding
-    let a_binding;
+    let mut _mutable_integer = 7i32;
 
     {
-        let x = 2;
+        // Shadowing by immutable `_mutable_integer`
+        let _mutable_integer = _mutable_integer;
 
-        // Initialize the binding
-        a_binding = x * x;
+        // Error! `_mutable_integer` is frozen in this scope
+        //_mutable_integer = 50;
+        // FIXME ^ Comment out this line
+
+        // `_mutable_integer` goes out of scope
     }
 
-    println!("a binding: {}", a_binding);
-
-    let another_binding;
-
-    // Error! Use of uninitialized binding
-    //println!("another binding: {}", another_binding);
-    // FIXME ^ Comment out this line
-
-    another_binding = 1;
-
-    println!("another binding: {}", another_binding);
+    // Ok! `_mutable_integer` is not frozen in this scope
+    _mutable_integer = 3;
 }
