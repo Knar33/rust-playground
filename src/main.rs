0a204ed6d501504a7cdbf080ac7eaa4be97d5a3b
Knar <knarxr@protonmail.com>
2024-02-21 17:54:25 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,27 @@
-fn main() {
-    // Make `optional` of type `Option<i32>`
-    let optional = Some(7);
+use std::str::FromStr;
 
-    match optional {
-        Some(i) => {
-            println!("This is a really long string and `{:?}`", i);
-            // ^ Needed 2 indentations just so we could destructure
-            // `i` from the option.
-        }
-        _ => {}
-        // ^ Required because `match` is exhaustive. Doesn't it seem
-        // like wasted space?
+fn get_count_item(s: &str) -> (u64, &str) {
+    let mut it = s.split(' ');
+    // let (Some(count_str), Some(item)) = (it.next(), it.next()) else {
+    //     panic!("Can't segment count item pair: '{s}'");
+    // };
+    // let Ok(count) = u64::from_str(count_str) else {
+    //     panic!("Can't parse integer: '{count_str}'");
+    // };
+
+    let (count_str, item) = match (it.next(), it.next()) {
+        (Some(count_str), Some(item)) => (count_str, item),
+        _ => panic!("Can't segment count ictem pair: '{s}'"),
+    };
+    let count = if let Ok(count) = u64::from_str(count_str) {
+        count
+    } else {
+        panic!("Can't parse integer: '{count_str}'");
     };
 
-    // All have type `Option<i32>`
-    let number = Some(7);
-    let letter: Option<i32> = None;
-    let emoticon: Option<i32> = None;
+    (count, item)
+}
 
-    // The `if let` construct reads: "if `let` destructures `number` into
-    // `Some(i)`, evaluate the block (`{}`).
-    if let Some(i) = number {
-        println!("Matched {:?}!", i);
-    }
-
-    // If you need to specify a failure, use an else:
-    if let Some(i) = letter {
-        println!("Matched {:?}!", i);
-    } else {
-        // Destructure failed. Change to the failure case.
-        println!("Didn't match a number. Let's go with a letter!");
-    }
-
-    // Provide an altered failing condition.
-    let i_like_letters = false;
-
-    if let Some(i) = emoticon {
-        println!("Matched {:?}!", i);
-    // Destructure failed. Evaluate an `else if` condition to see if the
-    // alternate failure branch should be taken:
-    } else if i_like_letters {
-        println!("Didn't match a number. Let's go with a letter!");
-    } else {
-        // The condition evaluated false. This branch is the default:
-        println!("I don't like letters. Let's go with an emoticon :)!");
-    }
+fn main() {
+    assert_eq!(get_count_item("3 chairs"), (3, "chairs"));
 }
