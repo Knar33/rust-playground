027528c2fac1d0a5df85fa59f6e885c5cf99fad4
Knar <knarxr@protonmail.com>
2024-02-26 16:42:23 -0500

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 fn main() {
-    let x = 5u32;
+    let n = 5;
 
-    let y = {
-        let x_squared = x * x;
-        let x_cube = x_squared * x;
+    if n < 0 {
+        print!("{} is negative", n);
+    } else if n > 0 {
+        print!("{} is positive", n);
+    } else {
+        print!("{} is zero", n);
+    }
 
-        // This expression will be assigned to `y`
-        x_cube + x_squared + x
+    let big_n = if n < 10 && n > -10 {
+        println!(", and is a small number, increase ten-fold");
+
+        // This expression returns an `i32`.
+        10 * n
+    } else {
+        println!(", and is a big number, halve the number");
+
+        // This expression must return an `i32` as well.
+        n / 2
+        // TODO ^ Try suppressing this expression with a semicolon.
     };
+    //   ^ Don't forget to put a semicolon here! All `let` bindings need it.
 
-    let z = {
-        // The semicolon suppresses this expression and `()` is assigned to `z`
-        2 * x;
-    };
-
-    println!("x is {:?}", x);
-    println!("y is {:?}", y);
-    println!("z is {:?}", z);
+    println!("{} -> {}", n, big_n);
 }
