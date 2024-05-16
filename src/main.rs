3190dbe8e10f0b6c4ba4a2bc5828c6552e03b173
Knar <knarxr@protonmail.com>
2024-05-16 16:45:11 -0400

    longest subsequence

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 fn main() {
-    println!("{}", is_palindrome(111));
+    println!("{}", find_length_of_lcis(vec![1,2,3,4,3,5] as Vec<i32>));
 }
 
-pub fn is_palindrome(x: i32) -> bool {
-    let chars: Vec<char> = x.to_string().chars().collect();
-    let length = chars.len();
-    for (index, &value) in chars.iter().enumerate() {
-        if value != chars[length - 1 - index] {
-            return false
+pub fn find_length_of_lcis(nums: Vec<i32>) -> i32 {
+    let mut longest: i32 = 1;
+    for i in 0..nums.len() - 1 {
+        let mut length: i32 = 1;
+        for j in i + 1..nums.len() {  
+            if nums[j] > nums[j-1] {
+                length += 1;
+            } else {
+                break;
+            }
+        }
+        if length > longest {
+            longest = length;
         }
     }
-    true
+    longest
 }
