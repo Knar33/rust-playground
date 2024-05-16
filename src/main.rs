91b9373018eb056ecde49891f5396117b68b3c81
Knar <knarxr@protonmail.com>
2024-05-16 16:43:18 -0400

    is palindrome

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", is_palindrome(111));
+}
+
+pub fn is_palindrome(x: i32) -> bool {
+    let chars: Vec<char> = x.to_string().chars().collect();
+    let length = chars.len();
+    for (index, &value) in chars.iter().enumerate() {
+        if value != chars[length - 1 - index] {
+            return false
+        }
+    }
+    true
 }
