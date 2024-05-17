179e770330cd3c0410fc014d4bbd07a5f1b00704
Knar <knarxr@protonmail.com>
2024-05-17 01:48:59 -0400

    invert binary tree

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+
 fn main() {
-    println!("{:?}", two_sum(vec![1,2,3,4,3,5] as Vec<i32>, 7i32));
+    let tree = Some(Rc::new(RefCell::new(TreeNode::new(222))));
+    println!("{:?}", invert_tree(tree));
 }
 
-pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-    for i in 0..nums.len() - 1 {
-        let num1 = nums[i];
-        for j in 0..nums.len() {
-            if i == j {
-                continue;
-            }
-            let num2 = nums[j];
-            if num1 + num2 == target {
-                return vec![i as i32, j as i32]
-            }
+pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+    match root {
+        Some(value) => {
+            let root_value = value.borrow();
+            let mut new_root = TreeNode::new(root_value.val);
+            new_root.left = invert_tree(root_value.right.clone());
+            new_root.right = invert_tree(root_value.left.clone());
+            Some(Rc::new(RefCell::new(new_root)))
         }
+        None => None
     }
-    unreachable!()
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
 }
