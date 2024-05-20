0d2ea3d60c6d1d13523effa989b850363bb75b20
Knar <knarxr@protonmail.com>
2024-05-20 14:17:59 -0400

    roman to int

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,33 @@
 use std::rc::Rc;
 use std::cell::RefCell;
 
-fn main() {
-    let tree = Some(Rc::new(RefCell::new(TreeNode::new(222))));
-    println!("{:?}", invert_tree(tree));
+fn main() { 
+    println!("{:?}", roman_to_int("MDCCLXXVI".to_string()));
 }
 
-pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-    if let Some(node) = &root {
-        let mut node_borrow = node.borrow_mut();
-        let left = node_borrow.left.take();
-        let right = node_borrow.right.take();
-        node_borrow.left = invert_tree(right);
-        node_borrow.right = invert_tree(left);
+pub fn roman_to_int(s: String) -> i32 {
+    let mut ret = 0;
+    let mut prev = 0;
+    for (i, current) in s.chars().enumerate() {
+    let current_int = match current {
+        'I' => 1,
+        'V' => 5,
+        'X' => 10,
+        'L' => 50,
+        'C' => 100,
+        'D' => 500,
+        'M' => 1000,
+        _ => 0
+    };
+    if i > 0 {
+        if (current_int > prev) {
+        ret -= prev
+        } else {
+        ret += prev
+        }
     }
-    root
-}
-
-#[derive(Debug, PartialEq, Eq)]
-pub struct TreeNode {
-  pub val: i32,
-  pub left: Option<Rc<RefCell<TreeNode>>>,
-  pub right: Option<Rc<RefCell<TreeNode>>>,
-}
-
-impl TreeNode {
-  #[inline]
-  pub fn new(val: i32) -> Self {
-    TreeNode {
-      val,
-      left: None,
-      right: None
+    prev = current_int;
     }
-  }
+    ret += prev;
+    ret
 }
