380d2852f4991db454740d76b8d402bf2c5101f3
Knar <knarxr@protonmail.com>
2024-05-21 01:13:20 -0400

    valid parenthesis

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,31 @@
-use std::rc::Rc;
-use std::cell::RefCell;
-
 fn main() { 
-    println!("{:?}", longest_common_prefix(vec!["reflower".to_string(),"flow".to_string(),"flight".to_string()]));
+    println!("{:?}", is_valid("([)]".to_string()));
 }
 
-pub fn longest_common_prefix(strs: Vec<String>) -> String {
-  let mut letters: Vec<char> = Vec::new();
-  let mut val: Option<usize> = None;
-  'outer: for str in strs {
-    let mut new_letters: Vec<char> = Vec::new();
-    for (i, ref char) in  str.chars().enumerate() {
-      if let Some(letter) = letters.get(i) {
-        if letter != char {
-          letters = new_letters.clone();
-          break
+pub fn is_valid(s: String) -> bool {
+  let mut openers: Vec<char> = Vec::new();
+
+  for char in s.chars() {
+    match char {
+      matched @ (']' | ')' | '}') => {
+        if let Some(popped) = openers.pop() {
+          if matched != invert(popped) {
+            return false
+          }
         }
-      }
-      new_letters.push(*char);
-    }
-    if let Some(value) = val {
-      if (new_letters.len() < value) {
-        val = Some(new_letters.len());
-        letters = new_letters.clone();
-      }
-    }
-    else {
-      val = Some(new_letters.len());
-      letters = new_letters.clone();
+      },
+      c => openers.push(c)
     }
   }
-  letters.into_iter().collect()
+    
+  return openers.len() == 0
+}
+
+fn invert(c: char) -> char {
+  match c {
+    '{' => '}',
+    '[' => ']',
+    '(' => ')',
+    _ => c
+  }
 }
