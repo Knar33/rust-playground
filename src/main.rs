b9d83a93ad44c428a0fcda627211f8a8ed1e531a
Knar <knarxr@protonmail.com>
2024-05-21 02:18:30 -0400

    merge two sorted linked lists

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
 fn main() { 
-    println!("{:?}", is_valid("([)]".to_string()));
+  let list1 = None;
+  let list2 = Some(Box::new(ListNode{next: None, val: 0}));
+
+  println!("{:?}", merge_two_lists(list1, list2));
 }
 
-pub fn is_valid(s: String) -> bool {
-  let mut openers: Vec<char> = Vec::new();
-
-  for char in s.chars() {
-    match char {
-      matched @ (']' | ')' | '}') => {
-        if let Some(popped) = openers.pop() {
-          if matched != invert(popped) {
-            return false
-          }
+pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    if let Some (listnode1) = list1.clone() {
+      if let Some(listnode2) = list2.clone() {
+        if listnode1.val < listnode2.val {
+            Some(Box::new(ListNode {
+              next: merge_two_lists(listnode1.next, list2),
+              val: listnode1.val
+            }))
+        } else {
+          Some(Box::new(ListNode {  next: merge_two_lists(list1, listnode2.next), val: listnode2.val }))
         }
-      },
-      c => openers.push(c)
+      } else {
+          Some(Box::new(ListNode { next: merge_two_lists(listnode1.next, None), val: listnode1.val }))
+      }
+    } else if let Some(listnode2) = list2.clone() {
+        Some(Box::new(ListNode { next: merge_two_lists(listnode2.next, None), val: listnode2.val }))
+    } else {
+      None
     }
-  }
-    
-  return openers.len() == 0
 }
 
-fn invert(c: char) -> char {
-  match c {
-    '{' => '}',
-    '[' => ']',
-    '(' => ')',
-    _ => c
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+  pub val: i32,
+  pub next: Option<Box<ListNode>>
+}
+
+impl ListNode {
+  #[inline]
+  fn new(val: i32) -> Self {
+    ListNode {
+      next: None,
+      val
+    }
   }
 }
