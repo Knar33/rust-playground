aae6b6ab491e682f85dc73ae6d6b3ed9e899e0c0
Knar <knarxr@protonmail.com>
2024-05-21 15:17:20 -0400

    remove duplicates

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,20 @@
 fn main() { 
-  let list1 = None;
-  let list2 = Some(Box::new(ListNode{next: None, val: 0}));
-
-  println!("{:?}", merge_two_lists(list1, list2));
+  println!("{:?}", remove_duplicates(&mut vec![0,0,1,1,1,2,2,3,3,4]))
 }
 
-pub fn merge_two_lists(list1: Option<Box<ListNode>>, list2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    if let Some (listnode1) = list1.clone() {
-      if let Some(listnode2) = list2.clone() {
-        if listnode1.val < listnode2.val {
-            Some(Box::new(ListNode {
-              next: merge_two_lists(listnode1.next, list2),
-              val: listnode1.val
-            }))
-        } else {
-          Some(Box::new(ListNode {  next: merge_two_lists(list1, listnode2.next), val: listnode2.val }))
-        }
-      } else {
-          Some(Box::new(ListNode { next: merge_two_lists(listnode1.next, None), val: listnode1.val }))
-      }
-    } else if let Some(listnode2) = list2.clone() {
-        Some(Box::new(ListNode { next: merge_two_lists(listnode2.next, None), val: listnode2.val }))
-    } else {
-      None
-    }
-}
+pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+let num_len = nums.len();
+  if nums.len() == 0 {
+    return 0
+  }
 
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
-}
-
-impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+  let mut j = 0;
+  for i in 0..num_len {
+    if nums[j] != nums[i] {
+      j += 1;
+      nums[j] = nums[i];
+      
     }
   }
+  j as i32 + 1
 }
