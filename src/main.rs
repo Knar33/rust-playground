52ece14b1e5ea6eaf8dc590514456641cbcb5084
Knar <knarxr@protonmail.com>
2024-05-22 01:28:59 -0400

    fixed add two numbers

--- src/main.rs
+++ src/main.rs
@@ -2,63 +2,62 @@ fn main() {
 }
 
 pub fn add_two_numbers(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-  let l3: &mut Option<Box<ListNode>> = &mut Some(Box::new(ListNode::new(0)));
-  let l1 = &mut l1.clone();
-  let l2 = &mut l2.clone();
-  add_two_numbers_recurse(l1, l2, l3, false)
+  let l3 = Some(Box::new(ListNode::new(0)));
+  let a = add_two_numbers_recurse(&l1, &l2, l3, false);
+  let b = 0;
+  return a
 }
 
-pub fn add_two_numbers_recurse(&mut l1: &mut Option<Box<ListNode>>, &mut l2: &mut Option<Box<ListNode>>, &mut l3: &mut Option<Box<ListNode>>, carry: bool) -> Option<Box<ListNode>> {
-  let mut l1_num = 0;
-  let mut l2_num  = 0;
+pub fn add_two_numbers_recurse(l1: &Option<Box<ListNode>>, l2: &Option<Box<ListNode>>, mut l3: Option<Box<ListNode>>, carry: bool) -> Option<Box<ListNode>> {
+  if let Some(node1) = l1 {
+    let l1_num = node1.val;
+    if let Some(node2) = l2 {
+      //both nodes exist
+      let l2_num = node2.val;
+      let mut sum = l1_num + l2_num + (if carry {1} else {0});
 
-  if let Some(ref node1) = l1 {
-    l1_num = node1.val;
-    l1 = node1.next;
-    if let Some(ref node2) = l2 {
-      l2_num = node2.val;
-      l2 = node2.next;
+      let mut next_carry = false;
+      if sum > 9 {
+        next_carry = true;
+        sum = sum - 10;
+      }
+      
+      let l3_mut = l3.as_mut().unwrap();
+      l3_mut.val = sum;
+      l3_mut.next = add_two_numbers_recurse(&node1.next, &node2.next, Some(Box::new(ListNode::new(0))), next_carry);
+      return l3;
+    } else {
+      //only one node exists
+      let l3_mut = l3.as_mut().unwrap();
+      l3_mut.val = node1.val + if carry {1} else {0};
+      let mut next_carry = false;
+      if l3_mut.val > 9 {
+        next_carry = true;
+        l3_mut.val = l3_mut.val - 10;
+      }
+      l3_mut.next = add_two_numbers_recurse(&node1.next, &None, Some(Box::new(ListNode::new(0))), next_carry);
+      return l3;
     }
   } else if let Some(node) = l2 {
-    l2_num = node.val;
-  } else {
-    return None
-  }
-
-  let mut sum = l1_num + l2_num;
-  if (carry) {
-    sum += 1;
-  }
-
-  if let Some(node) = l3 {
-    if sum >= 10 {
-      node.val = sum - 10;
-      l3 = node.next;
-      return add_two_numbers_recurse(&mut l1, &mut l2, &mut l3, true)
-    } else {
-      node.val = sum;
-      l3 = node.next;
-      return add_two_numbers_recurse(&mut l1, &mut l2, &mut l3, false)
+    //only one node exists
+    let l3_mut = l3.as_mut().unwrap();
+    l3_mut.val = node.val + if carry {1} else {0};
+    let mut next_carry = false;
+    if l3_mut.val > 9 {
+        next_carry = true;
+        l3_mut.val = l3_mut.val - 10;
     }
+    l3_mut.next = add_two_numbers_recurse(&None, &node.next, Some(Box::new(ListNode::new(0))), next_carry);
+    return l3;
   } else {
-    return None
-  }
-
-}
-
-
-#[derive(PartialEq, Eq, Clone, Debug)]
-pub struct ListNode {
-  pub val: i32,
-  pub next: Option<Box<ListNode>>
-}
-
-impl ListNode {
-  #[inline]
-  fn new(val: i32) -> Self {
-    ListNode {
-      next: None,
-      val
+    //both empty nodes
+    if carry {
+        let l3_mut = l3.as_mut().unwrap();
+        l3_mut.val = if carry {1} else {0};
+        l3_mut.next = None;
+        return l3;
+    } else {
+      None
     }
   }
 }
@@ -85,7 +84,10 @@ mod tests {
 
     #[test]
     fn asserts() {
-        assert_eq!(search_insert(vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4), 4);
-        assert_eq!(search_insert(vec![0, 1, 2, 3, 5, 6, 7, 8, 9, 10], 4), 4);
+      let a = add_two_numbers(
+        Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: None}))}))}))}))}))}))})),
+        Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: Some(Box::new(ListNode{val: 9, next: None}))}))}))}))
+      );
+      assert_eq!(a.unwrap().val, 7);
     }
 }
