d299e3408c64aaca658414f60caa84c5fa1ccfb9
Knar <knarxr@protonmail.com>
2024-05-23 18:45:05 -0400

    zigzag convert

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,76 @@
-use std::collections::HashMap;
-
 fn main() { 
 }
 
-pub fn length_of_longest_substring(s: String) -> i32 {
-  let s_len = s.len() as i32;
-  if s_len < 2 {
-    return s_len
-  }
+pub fn convert(s: String, num_rows: i32) -> String {
+  let mut output_string: Vec<u8> = Vec::new();
+  let mut num_columns = 0;
+  let mut column_iterator = 0;
+  let s_len = s.len();
 
-  let mut longest = 0;
-  let mut i = 0;
-  let mut j = 0;
+  while column_iterator < s_len {
+    let mut i = 0;
+    while i < num_rows {
+      column_iterator += 1;
+      i += 1;
+    }
 
-  //left pointer 
-  while i < s_len {
-    let mut chars: HashMap<u8, i32> = HashMap::with_capacity(512);
-    let mut length = 1;
-    //right pointer
-    while j < s_len {
-      if j == i {
-        //second pointer is at first position
-        chars.insert(s.as_bytes()[j as usize], j);
-        j += 1;
-        continue;
-      } else {
-        //second pointer is ahead of first and in a valid spot
-        let j_char = s.as_bytes()[j as usize];
-        if chars.contains_key(&j_char) {
-          i = *chars.get(&j_char).unwrap() + 1;
-          j = i;
-          break;
+    num_columns += 1;
+
+    if num_rows > 2 {
+      let rows_up = num_rows - 2;
+      let mut i = 0;
+      while i < rows_up {
+        if column_iterator < s_len {
+          num_columns += 1;
+          column_iterator += 1;
         }
-        chars.insert(j_char, j);
-        length += 1;
-        j += 1;
+        i += 1;
       }
     }
-    if length > longest  {
-      longest = length;
+  }
+
+  // let mut matrix: Vec<Vec<u8>> = Vec::new();
+  let mut matrix: Vec<Vec<u8>> = vec![vec![b' '; num_columns]; num_rows as usize];
+  let mut column = 0;
+  let mut iterator = 0;
+  while iterator < s_len {
+    //zig down
+    let mut i = 0;
+    while i < num_rows {
+      if iterator < s_len {
+        matrix[i as usize][column] = s.as_bytes()[iterator];
+        iterator += 1;
+      }
+      i += 1;
     }
-    if j == s_len {
-      break
+
+    column += 1;
+
+    //zig back up
+    if num_rows > 2 {
+      let rows_up = num_rows - 2;
+      let mut i = 0;
+      while i < rows_up {
+        if iterator < s_len {
+          matrix[(num_rows - 2 - i) as usize][column] = s.as_bytes()[iterator];
+          iterator += 1;
+          column += 1;
+        }
+        i += 1;
+      }
     }
   }
-  longest
+
+  let mut i: usize = 0; while i < num_rows as usize {
+    let mut j: usize = 0; while j < num_columns {
+      if matrix[i][j] != b' ' {
+        output_string.push(matrix[i][j]);
+      }
+      j += 1;
+    }
+    i += 1;
+  }
+  String::from_utf8(output_string).unwrap()
 }
 
 #[cfg(test)]
@@ -53,6 +79,6 @@ mod tests {
 
     #[test]
     fn asserts() {
-      assert_eq!(length_of_longest_substring("dvdf".to_string()), 2);
+      assert_eq!(convert("PAYPALISHIRING".to_string(), 4), "PINALSIGYAHRPI".to_string());
     }
 }
