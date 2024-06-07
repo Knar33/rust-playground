5269811166f3339a975e60ae7df5e8246edc357f
Knar <knarxr@protonmail.com>
2024-06-06 21:54:35 -0400

    Rust by example.

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,54 @@
+// A function which takes a closure as an argument and calls it.
+// <F> denotes that F is a "Generic type parameter"
+fn apply<F>(f: F)
+where
+    // The closure takes no input and returns nothing.
+    F: FnOnce(),
+{
+    // ^ TODO: Try changing this to `Fn` or `FnMut`.
+
+    f();
+}
+
+// A function which takes a closure and returns an `i32`.
+fn apply_to_3<F>(f: F) -> i32
+where
+    // The closure takes an `i32` and returns an `i32`.
+    F: Fn(i32) -> i32,
+{
+    f(3)
+}
+
 fn main() {
     use std::mem;
 
-    let color = String::from("green");
-
-    // A closure to print `color` which immediately borrows (`&`) `color` and
-    // stores the borrow and closure in the `print` variable. It will remain
-    // borrowed until `print` is used the last time.
-    //
-    // `println!` only requires arguments by immutable reference so it doesn't
-    // impose anything more restrictive.
-    let print = || println!("`color`: {}", color);
-
-    // Call the closure using the borrow.
-    print();
-
-    // `color` can be borrowed immutably again, because the closure only holds
-    // an immutable reference to `color`.
-    let _reborrow = &color;
-    print();
-
-    // A move or reborrow is allowed after the final use of `print`
-    let _color_moved = color;
-
-    let mut count = 0;
-    // A closure to increment `count` could take either `&mut count` or `count`
-    // but `&mut count` is less restrictive so it takes that. Immediately
-    // borrows `count`.
-    //
-    // A `mut` is required on `inc` because a `&mut` is stored inside. Thus,
-    // calling the closure mutates `count` which requires a `mut`.
-    let mut inc = || {
-        count += 1;
-        println!("`count`: {}", count);
-    };
-
-    // Call the closure using a mutable borrow.
-    inc();
-
-    // The closure still mutably borrows `count` because it is called later.
-    // An attempt to reborrow will lead to an error.
-    // let _reborrow = &count;
-    // ^ TODO: try uncommenting this line.
-    inc();
-
-    // The closure no longer needs to borrow `&mut count`. Therefore, it is
-    // possible to reborrow without an error
-    let _count_reborrowed = &mut count;
-
+    let greeting = "hello";
     // A non-copy type.
-    let movable = Box::new(3);
+    // `to_owned` creates owned data from borrowed one
+    let mut farewell = "goodbye".to_owned();
 
-    // `mem::drop` requires `T` so this must take by value. A copy type
-    // would copy into the closure leaving the original untouched.
-    // A non-copy must move and so `movable` immediately moves into
-    // the closure.
-    let consume = || {
-        println!("`movable`: {:?}", movable);
-        mem::drop(movable);
+    // Capture 2 variables: `greeting` by reference and
+    // `farewell` by value.
+    let diary = || {
+        // `greeting` is by reference: requires `Fn`.
+        println!("I said {}.", greeting);
+
+        // Mutation forces `farewell` to be captured by
+        // mutable reference. Now requires `FnMut`.
+        farewell.push_str("!!!");
+        println!("Then I screamed {}.", farewell);
+        println!("Now I can sleep. zzzzz");
+
+        // Manually calling drop forces `farewell` to
+        // be captured by value. Now requires `FnOnce`.
+        mem::drop(farewell);
     };
 
-    // `consume` consumes the variable so this can only be called once.
-    consume();
-    // consume();
-    // ^ TODO: Try uncommenting this line.
+    // Call the function which applies the closure.
+    apply(diary);
+
+    // `double` satisfies `apply_to_3`'s trait bound
+    let double = |x| 2 * x;
+
+    println!("3 doubled: {}", apply_to_3(double));
 }
