df1ff6ece67d8cfce099502108a15f926adb5330
Knar <knarxr@protonmail.com>
2024-09-10 18:48:04 -0400

    Rust by example

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,31 @@
-// A trait which implements the print marker: `{:?}`.
-use std::fmt::Debug;
+struct Cardinal;
+struct BlueJay;
+struct Turkey;
 
-trait HasArea {
-    fn area(&self) -> f64;
-}
+trait Red {}
+trait Blue {}
 
-impl HasArea for Rectangle {
-    fn area(&self) -> f64 {
-        self.length * self.height
-    }
-}
+impl Red for Cardinal {}
+impl Blue for BlueJay {}
 
-#[derive(Debug)]
-struct Rectangle {
-    length: f64,
-    height: f64,
+// These functions are only valid for types which implement these
+// traits. The fact that the traits are empty is irrelevant.
+fn red<T: Red>(_: &T) -> &'static str {
+    "red"
 }
-#[allow(dead_code)]
-struct Triangle {
-    length: f64,
-    height: f64,
-}
-
-// The generic `T` must implement `Debug`. Regardless
-// of the type, this will work properly.
-fn print_debug<T: Debug>(t: &T) {
-    println!("{:?}", t);
-}
-
-// `T` must implement `HasArea`. Any type which meets
-// the bound can access `HasArea`'s function `area`.
-fn area<T: HasArea>(t: &T) -> f64 {
-    t.area()
+fn blue<T: Blue>(_: &T) -> &'static str {
+    "blue"
 }
 
 fn main() {
-    let rectangle = Rectangle {
-        length: 3.0,
-        height: 4.0,
-    };
-    let _triangle = Triangle {
-        length: 3.0,
-        height: 4.0,
-    };
+    let cardinal = Cardinal;
+    let blue_jay = BlueJay;
+    let _turkey = Turkey;
 
-    print_debug(&rectangle);
-    println!("Area: {}", area(&rectangle));
-
-    //print_debug(&_triangle);
-    //println!("Area: {}", area(&_triangle));
-    // ^ TODO: Try uncommenting these.
-    // | Error: Does not implement either `Debug` or `HasArea`.
+    // `red()` won't work on a blue jay nor vice versa
+    // because of the bounds.
+    println!("A cardinal is {}", red(&cardinal));
+    println!("A blue jay is {}", blue(&blue_jay));
+    //println!("A turkey is {}", red(&_turkey));
+    // ^ TODO: Try uncommenting this line.
 }
